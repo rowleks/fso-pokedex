316827922ee53c6c36f7e336f65631a248bdce75
Rowland Momoh <momohrowland@gmail.com>
2026-02-19 11:03:43 +0100

    Fix build and pipeline error

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 
 // get the port from env variable
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
+
+// Handle client-side routing - serve index.html for all routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+})
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
